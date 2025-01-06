b7b609904bd11a3a6b83299bdf6e884a7211c705
ardra-vijayan28 <ardravijayan28@gmail.com>
2025-01-06 13:25:09 +0530

    User Profile and Forms

--- Tracker_frontend/src/App.jsx
+++ Tracker_frontend/src/App.jsx
@@ -1,13 +1,19 @@
 
 import './App.css'
 import Tech_Dashboard from './components/Tech_Dashboard'
+// import Delete from './components/Delete'
+// import Form from './components/Form'
+// import User_profile from './components/User_profile'
+// import Update from './components/Update'
 
 function App() {
 
 
   return (
+  <>
     <Tech_Dashboard/>
-    
+      {/* <User_profile/> */}
+    </>
   )
 }
 
