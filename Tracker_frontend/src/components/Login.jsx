6ef35384338129f630b782139011d1a835386646
ardra-vijayan28 <ardravijayan28@gmail.com>
2025-01-06 17:55:26 +0530

    Login added

---
+++ Tracker_frontend/src/components/Login.jsx
@@ -0,0 +1,17 @@
+function Login(){
+    return(
+        <>
+        <label>Name</label>
+        <br/>
+        <input name="Name" placeholder="Enter your Name"></input>
+        <br/>
+        <br/>
+        <label>Password</label>
+        <input name="Password" placeholder="Enter Password"></input>
+        <br/>
+        <br/>
+        <button>Login</button>
+        </>
+    )
+}
+export default Login;
