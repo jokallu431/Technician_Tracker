bd5115e2064bb940620556276dafd6d2872aa851
Joshua <joshua@reflexion.ai>
2025-01-06 13:13:14 +0530

    Dummy Dashboard Ui

--- Tracker_frontend/src/App.jsx
+++ Tracker_frontend/src/App.jsx
@@ -1,19 +1,13 @@
 
 import './App.css'
-// import Delete from './components/Delete'
-// import Form from './components/Form'
-import User_profile from './components/User_profile'
-// import Update from './components/Update'
-import Tech_Dashboard from './Tech_Dashboard'
+import Tech_Dashboard from './components/Tech_Dashboard'
 
 function App() {
 
 
   return (
-    <>
-       <User_profile/>
-      <Tech_Dashboard/>
-    </>
+    <Tech_Dashboard/>
+    
   )
 }
 
