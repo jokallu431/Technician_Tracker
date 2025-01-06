173e8dc84ccbb25b4162a60f3451789d3b2593d0
ardra-vijayan28 <ardravijayan28@gmail.com>
2025-01-06 13:25:09 +0530

    User Profile and Forms

--- Tracker_frontend/src/App.jsx
+++ Tracker_frontend/src/App.jsx
@@ -6,35 +6,16 @@ import Login from './components/Login'
 // import Tech_Dashboard from './components/Tech_Dashboard'
 // import Delete from './components/Delete'
 // import Form from './components/Form'
-// import User_profile from './components/User_profile'
+import User_profile from './components/User_profile'
 // import Update from './components/Update'
+
 
 function App() {
   
 
   return (
-  <>
-  {/* <div className='task'> 
-  <Login/>
-  <Layout/>
-   <div id="wrapper">
-    <Tech_Dashboard/>
-    </div>
+    <>
       <User_profile/>
-      </div> */}
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout/>}>
-              <Route index="/login"  element={<Login/>}></Route>
-              <Route path="/login"  element={<Login/>}></Route>
-              <Route path="/signup"  element={<Signup/>}></Route>
-              <Route path="/forgetpassword"  element={<ForgetPassword/>}></Route>
-              <Route path="/dashboard"  element={<Tech_Dashboard/>}></Route>
-          </Route>
-          
-        </Routes>
-    </BrowserRouter>
     </>
   )
 }
