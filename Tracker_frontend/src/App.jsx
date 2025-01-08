745ead8e47951fe7a2317e3e3aa359f8918b76c5
Joshua <joshua@reflexion.ai>
2025-01-08 13:04:06 +0530

    Layout Split for dashboard

--- Tracker_frontend/src/App.jsx
+++ Tracker_frontend/src/App.jsx
@@ -1,7 +1,11 @@
 
 import './App.css'
+import ForgetPassword from './components/ForgetPassword';
+import Layout from './components/Layout'
 import Login from './components/Login'
+import Signup from './components/Signup';
 import Tech_Dashboard from './components/Tech_Dashboard'
+import {BrowserRouter, Routes, Route} from 'react-router';
 // import Delete from './components/Delete'
 // import Form from './components/Form'
 // import User_profile from './components/User_profile'
@@ -12,11 +16,27 @@ function App() {
 
   return (
   <>
-  <div className='task'> 
+  {/* <div className='task'> 
   <Login/>
-    {/*<Tech_Dashboard/>*/}
-      {/* <User_profile/> */}
-      </div>
+  <Layout/>
+   <div id="wrapper">
+    <Tech_Dashboard/>
+    </div>
+      <User_profile/>
+      </div> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+              <Route index="/login"  element={<Login/>}></Route>
+              <Route path="/login"  element={<Login/>}></Route>
+              <Route path="/signup"  element={<Signup/>}></Route>
+              <Route path="/forgetpassword"  element={<ForgetPassword/>}></Route>
+              <Route path="/dashboard"  element={<Tech_Dashboard/>}></Route>
+          </Route>
+          
+        </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- Tracker_frontend/src/components/Layout.jsx
+++ Tracker_frontend/src/components/Layout.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-
+import React from 'react';
+import { Outlet } from 'react-router';
 const Layout = () => {
   return (
-    <div>Layout</div>
+   <>
+      <Outlet/>
+   </>
   )
 }
 
